8f59393cadbbebaff4189eac9af0a5dc03704e7d
sherihanAlshareef <sherihan8alshareef@gmail.com>
2022-09-13 21:48:10 +0300

    22 years old commit

--- src/main.java
+++ src/main.java
@@ -16,6 +16,7 @@ public class main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello, i am sherihan");
+        System.out.println("Hello, i am 22 years old");
                 
     }
     
